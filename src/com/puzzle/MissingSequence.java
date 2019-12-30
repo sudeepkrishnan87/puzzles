08a521f8afc2c5eba96a81a29a1392beb745e2af
sudee <sudeepkrishnan87@gmail.com>
2019-12-30 22:35:48 +0530

    pulle to find missing sequence

---
+++ src/com/puzzle/MissingSequence.java
@@ -0,0 +1,17 @@
+package com.puzzle;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MissingSequence {
+    public static void main(String[] args) {
+
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 9, 10};
+        int largest = 10;
+        int expectedTotal = (10 * (10 + 1)) / 2;
+        int actualTotal = Arrays.stream(num).sum();
+        System.out.println("Missing number is"+ (expectedTotal-actualTotal));
+    }
+
+
+}
